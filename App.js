ebb6cb6e2e8626dc7c7b33b8bae3288fc6b5532a
KimRuedasR <kruedas09@alumnos.uaq.mx>
2023-09-25 13:39:19 -0600

    feat(Structure): add TextInput, Button and View elements

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [inputValue, setInputValue] = useState(''); // State to store input value
+
+  // Function to handle button press
+  const handleButtonPress = () => {
+    alert(`You entered: ${inputValue}`);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Hello world</Text>
+
+      <Text>DAM</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Escribe algo"
+        onChangeText={(text) => setInputValue(text)}
+        value={inputValue}
+      />
+
+      <Button title="Presioname" onPress={handleButtonPress} />
+
+      <View style={styles.box}></View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +38,23 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 16,
+  },
+  input: {
+    height: 40,
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 16,
+    paddingHorizontal: 8,
+  },
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'blue',
+    marginBottom: 16,
   },
 });

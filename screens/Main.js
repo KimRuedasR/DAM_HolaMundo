d2a11a8344a966e5867c48fc2f4469b104b6c760
KimRuedasR <kruedas09@alumnos.uaq.mx>
2023-10-23 20:24:43 -0600

    feat(A23): add DogAPI integration

--- screens/Main.js
+++ screens/Main.js
@@ -1,10 +1,11 @@
-import React, { useEffect } from "react";
-import { StyleSheet, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, View, FlatList, Image } from "react-native";
 import { StatusBar } from "expo-status-bar";
-import CustomButton from "../components/CustomButton";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+// import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function Main({ navigation }) {
+  const [dogs, setDogs] = useState([]);
+
   useEffect(function () {
     navigation.setOptions({
       headerShown: true,
@@ -13,20 +14,46 @@ export default function Main({ navigation }) {
         backgroundColor: "blue",
       },
     });
+    loadDogs();
   });
 
-  async function loadData() {
-    const data = await AsyncStorage.getItem("data");
-    console.log(data);
+  async function loadDogs() {
+    const response = await fetch("https://dog.ceo/api/breeds/image/random/50");
+    const data = await response.json();
+    const d = data.message.map((url, id) => {
+      return {
+        id: id,
+        url: url,
+      };
+    });
+    setDogs(d);
   }
+
+  const Item = (props) => (
+    <View style={styles.item}>
+      <Image
+        style={{ width: 200, height: 200 }}
+        source={{
+          uri: props.url,
+        }}
+      />
+    </View>
+  );
+
+  const renderItem = ({ item }) => <Item url={item.url} />;
 
   return (
     <View style={styles.container}>
-      <CustomButton text={"Regresar"} color={"blue"} action={loadData} />
+      <FlatList data={dogs} renderItem={renderItem} />
       <StatusBar style={"light"} />
     </View>
   );
 }
+
+// async function loadData() {
+//   const data = await AsyncStorage.getItem("data");
+//   console.log(JSON.parse(data));
+// }
 
 const styles = StyleSheet.create({
   container: {
@@ -34,5 +61,14 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  item: {
+    backgroundColor: "#f9c2ff",
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
   },
 });
